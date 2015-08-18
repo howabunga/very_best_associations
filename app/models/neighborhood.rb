2fb4d9831210ff6877931fbea3eec41e654ba51f
howabunga <weihowa@gmail.com>
2015-08-17 23:06:09 -0500

    everything but quick form add

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -3,4 +3,6 @@ class Neighborhood < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => {:scope => :city }
 
   has_many :venues
+  has_many :favorites, :through => :venues
+
 end
